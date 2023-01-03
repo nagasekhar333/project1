3db08eff4ddec8cfa08f816108e2d78e8f4d7ac0
nagasekhar333 <111780117+nagasekhar333@users.noreply.github.com>
2023-01-03 13:17:51 +0530

    Update testserver.tf

--- testserver.tf
+++ testserver.tf
@@ -4,9 +4,9 @@ provider "aws" {
 resource "aws_instance" "production_server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
-  key_name = "sri-key"
-   subnet_id = "subnet-0f840cec82fced399"
-   security_groups = ["sg-0e80014777a0f2a6a"]
+  key_name = "jenkins"
+   subnet_id = "subnet-0d9fb418acaf32010"
+   security_groups = ["sg-0c6da82a9d1e57a89"]
   tags = {
     Env = "production"
     Name = "production_server"
