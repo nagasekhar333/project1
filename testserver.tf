825127cc43fd7ba4ab4ed0bd9fb2a7e11f7470ff
nagasekhar333 <111780117+nagasekhar333@users.noreply.github.com>
2023-01-04 18:27:10 +0530

    Update testserver.tf

--- testserver.tf
+++ testserver.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "test-server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
-  key_name = "jenkins"
+  key_name = "ansi-jenkins"
    subnet_id = "subnet-0d9fb418acaf32010"
    security_groups = ["sg-0c6da82a9d1e57a89"]
   tags = {
